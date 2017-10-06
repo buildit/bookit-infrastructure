a1d461986e1b9ccc8b21492eebe848a5cdb3f345
Mike Thomas <mathomas@users.noreply.github.com>
2017-10-06 16:29:53 -0600

    Fail whole creation of riglet if any step fails. Fix issues with --region and quoting in wait commands.

--- create-standard-riglet.sh
+++ create-standard-riglet.sh
@@ -10,12 +10,11 @@ read -p "Are you sure you want to proceed?  " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Y" | make create-foundation ENV=integration
-  echo "Y" | make create-compute ENV=integration
-  echo "Y" | make create-foundation ENV=staging
-  echo "Y" | make create-compute ENV=staging
-  echo "Y" | make create-foundation ENV=production
-  echo "Y" | make create-compute ENV=production
-
+  echo "Y" | make create-foundation ENV=integration && \
+  echo "Y" | make create-compute ENV=integration && \
+  echo "Y" | make create-foundation ENV=staging && \
+  echo "Y" | make create-compute ENV=staging && \
+  echo "Y" | make create-foundation ENV=production && \
+  echo "Y" | make create-compute ENV=production && \
   echo "Y" | make create-build REPO=bookit-api REPO_BRANCH=master CONTAINER_PORT=8080 LISTENER_RULE_PRIORITY=10
 fi
