3a7e1c1c5e6fa8bd1193c28d044e565986865f45
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-10-24 15:33:25 -0500

    add delete-deps
    BKIT-55

--- delete-standard-riglet.sh
+++ delete-standard-riglet.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+oo#!/usr/bin/env bash
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
@@ -33,4 +33,5 @@ then
   deleteEnvironment integration
   deleteEnvironment staging
   deleteEnvironment production
+  echo "Y" | make delete-deps
 fi
