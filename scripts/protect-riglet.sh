fdd612a590113ed0cc690be01b42e64793b8cbf5
Chih-Ho Andy Chou <chihho.chou@wipro.com>
2018-05-15 20:07:50 -0400

    - Fixed issues when running protect/unprotected riglet scripts on regions other than default region by adding a region parameter to protect-riglet.sh

--- scripts/protect-riglet.sh
+++ scripts/protect-riglet.sh
@@ -1,25 +1,26 @@
-if [[ $# -lt 2 ]] ; then
-  echo "Usage: protect-riglet <prefix> <enable|disable|list> "
+if [[ $# -lt 3 ]] ; then
+  echo "Usage: protect-riglet <prefix> <region> <enable|disable|list>"
   echo "('list' just shows the stacks that would be impacted by 'enable' or 'disable')"
   exit 0
 fi
 
 prefix=${1}
-mode=${2}
+region=${2}
+mode=${3}
 
 list_stacks() {
-  aws cloudformation list-stacks --stack-status CREATE_COMPLETE UPDATE_COMPLETE | \
+  aws cloudformation list-stacks --stack-status CREATE_COMPLETE UPDATE_COMPLETE --region ${2}  | \
     jq '.StackSummaries[] | select(.StackName | startswith('\"${1}\"')) | select(has("ParentId") | not) | select(.StackName | index("-app-") | not) | .StackName' | sed 's/\"//g'
 }
 
-for stack in $(list_stacks ${prefix}); do
+for stack in $(list_stacks ${prefix} ${region}); do
   case "${mode}" in
-    enable) echo "Enabling protection: ${stack} "; aws cloudformation update-termination-protection --enable-termination-protection --stack-name $stack;
+    enable) echo "Enabling protection: ${stack} "; aws cloudformation update-termination-protection --enable-termination-protection --stack-name $stack --region ${region};
             ;;
-    disable) echo "Disabling protection ${stack}"; aws cloudformation update-termination-protection --no-enable-termination-protection --stack-name $stack;
+    disable) echo "Disabling protection ${stack}"; aws cloudformation update-termination-protection --no-enable-termination-protection --stack-name $stack --region ${region};
              ;;
     list) echo ${stack}
          ;;
-    *) echo "Invalid mode: ${mode} for ${stack}"
+    *) echo "Invalid mode: ${mode} for ${stack} at ${region}"
   esac
 done
