6b604b2d99325d934e61547444c74bbcac683d8f
Mike Thomas <mathomas@users.noreply.github.com>
2018-06-13 15:51:01 -0600

    Don't default Cert Manager GUID.

--- scripts/build-dotmake.sh
+++ scripts/build-dotmake.sh
@@ -9,7 +9,7 @@ read -p 'Owner of riglet: ' owner
 read -p 'AWS Profile ("default"): ' profile
 read -p 'Project ("bookit"): ' project
 read -p 'AWS region ("us-east-1"): ' region
-read -p 'AWS Certificate Manager GUID ("buildit.tools cert GUID"): ' domain_guid
+read -p 'AWS Certificate Manager GUID (NO DEFAULT.  Use buildit.tools cert GUID for your region.): ' domain_guid
 echo
 
 cat << EOF > .make
@@ -19,7 +19,7 @@ OWNER = ${owner}
 PROFILE = ${profile:-default}
 PROJECT = ${project:-bookit}
 REGION = ${region:-us-east-1}
-DOMAIN_CERT = ${domain_guid:-0663e927-e990-4157-aef9-7dea87faa6ec}
+DOMAIN_CERT = ${domain_guid}
 EOF
 
 echo 'Saved .make!'
